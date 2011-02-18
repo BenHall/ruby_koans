b3ebe758b39e609a20eea8079dc19f2a72c2e621
Ben Hall <Ben@Benhall.me.uk>
2011-02-18 12:22:39 +0000

    Triangle and exceptions

--- triangle.rb
+++ triangle.rb
@@ -15,14 +15,28 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
-	number_of_sides_equal = 0
-	number_of_sides_equal += 1 if (a == b)
-	number_of_sides_equal += 1 if (b == c)
-	number_of_sides_equal += 1 if (a == c)
+	validate_sides(a,b,c)
+	number_of_sides_equal = calculate_number_of_equal_sides(a,b,c)
 
 	return :equilateral if number_of_sides_equal == 3
 	return :isosceles if number_of_sides_equal == 1
 	return :scalene if number_of_sides_equal == 0
+end
+
+def validate_sides(a,b,c)
+	valid = true
+	valid = false if (a <= 0 || b <= 0 || c <= 0)
+	valid = false if (a == 1 && b == 1 && c != 1) || (a == 2 && c == 2 && b != 2)
+
+	raise TriangleError unless valid
+end
+
+def	calculate_number_of_equal_sides(a,b,c)
+	number_of_sides_equal = 0
+	number_of_sides_equal += 1 if (a == b)
+	number_of_sides_equal += 1 if (b == c)
+	number_of_sides_equal += 1 if (a == c)
+	return number_of_sides_equal
 end
 
 # Error class used in part 2.  No need to change this code.
